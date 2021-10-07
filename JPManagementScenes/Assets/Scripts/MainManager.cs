a7a91750e3a18013ec0441b12d232af9b882338a
Jorge Esteban Santillan <jestebansanti@gmail.com>
2021-10-06 23:24:15 -0700

    Implement data persistence between scenes: The script that persist the data between is done

---
+++ JPManagementScenes/Assets/Scripts/MainManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    public static MainManager Instance;
+    public Color teamColor;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        Instance=this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
